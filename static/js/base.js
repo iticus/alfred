18ebf9c27d45b3571de645af56d138875db11ff8
iticus <ticus.ionut@gmail.com>
2018-06-09 06:55:54 +0300

    improved PWA: added cache, install banner, colors and logo

--- static/js/base.js
+++ static/js/base.js
@@ -214,3 +214,15 @@ $(document).ready(function() {
 		$('#camdata').hide();
 	});
 });
+
+let installPromptEvent;
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent Chrome <= 67 from automatically showing the prompt
+  //event.preventDefault();
+  // Stash the event so it can be triggered later.
+  //installPromptEvent = event;
+  // Update the install UI to notify the user app can be installed
+  //document.querySelector('#install-button').disabled = false;
+  event.prompt();
+});
--- static/service-worker.js
+++ static/service-worker.js
@@ -7,3 +7,27 @@ self.addEventListener("push", function(event) {
     })
   );
 });
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll([
+            "/",
+            "/static/css/base.css",
+            "/static/js/base.js",
+            "/static/js/jquery-3.2.1.min.js",
+            "/static/img/logo_192.png"
+        ]);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
